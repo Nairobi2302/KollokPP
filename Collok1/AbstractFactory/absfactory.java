1b33f47d72afbc098af5052c8c5a81daf051286a
Lia_Yuna <137928950+Nairobi2302@users.noreply.github.com>
2024-01-11 23:30:21 +0300

    Update absfactory.java

--- Collok1/AbstractFactory/absfactory.java
+++ Collok1/AbstractFactory/absfactory.java
@@ -1,1 +1,23 @@
-Hello1
+package abstractfactory;
+
+public class absfactory {
+    public static void main(String[] args) {
+        // Используем абстрактную фабрику для создания элементов пользовательского интерфейса для Windows
+        GUIFactory windowsFactory = new WindowsFactory();
+        button windowsButton = windowsFactory.createButton();
+        Checkbox windowsCheckbox = windowsFactory.createCheckbox();
+
+        // Отображаем элементы пользовательского интерфейса для Windows
+        windowsButton.render();
+        windowsCheckbox.render();
+
+        // Используем абстрактную фабрику для создания элементов пользовательского интерфейса для Mac
+        GUIFactory macFactory = new MacFactory();
+        button macButton = macFactory.createButton();
+        Checkbox macCheckbox = macFactory.createCheckbox();
+
+        // Отображаем элементы пользовательского интерфейса для Mac
+        macButton.render();
+        macCheckbox.render();
+    }
+}
