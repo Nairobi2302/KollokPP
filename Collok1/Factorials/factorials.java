d4185089c37cc9de796e516962b355281378a52a
Lia_Yuna <137928950+Nairobi2302@users.noreply.github.com>
2024-01-11 22:58:35 +0300

    Update factorials.java

--- Collok1/Factorials/factorials.java
+++ Collok1/Factorials/factorials.java
@@ -1,1 +1,21 @@
-Hello
+package Factorials1;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+public class factorials {
+    public static void main(String[] args) {
+        try {
+            factcalc calculator = new factcalc();
+            int n = 10;
+            List<BigInteger> factorials = calculator.calculateFactorials(n);
+            for (int i = 0; i < factorials.size(); i++) {
+                System.out.println(factorials.get(i));
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
